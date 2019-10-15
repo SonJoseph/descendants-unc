1a604fe776e5ae9d17a08fdfc7e1a8e31ac0767c
SonJoseph <joeson34@cs.unc.edu>
2019-10-14 23:33:37 -0400

    reactjs served by python

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__, static_folder='client/build')
 
-if __name__ == "__main__":
-    app.run()
+# Serve React App
+@app.route('/', defaults={'path': ''}) 
+@app.route('/<path:path>') 
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000)
