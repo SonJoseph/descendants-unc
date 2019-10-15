6e0ce2c35dd2cae4b07241b7a3b768d5dbdf3e06
SonJoseph <joeson34@cs.unc.edu>
2019-10-14 23:39:53 -0400

    remove /build from master (production) gitignore

---
+++ client/build/precache-manifest.e8a4f44c6764da58a7e5a55a41fe098a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fd708f54dc3ca7d2af777b2d1557bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14211e329d9d041336f2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "14f7ba62d534ca10bdba",
+    "url": "/static/js/2.8e9d5bf3.chunk.js"
+  },
+  {
+    "revision": "14211e329d9d041336f2",
+    "url": "/static/js/main.2dea4df9.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
