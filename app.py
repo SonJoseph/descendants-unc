6c8025f5da1575ffb2fb1ad53ada03fdf13de0fe
SonJoseph <joeson34@cs.unc.edu>
2019-10-15 14:22:56 -0400

    React integration of GET nodes from neo4j

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from flask import jsonify
 
 app = Flask(__name__, static_folder='client/build')
 
+def connect(): # connect to neo4j instance 
+    graphenedb_url = "bolt://hobby-ghjkfkgldghkgbkegepiladl.dbs.graphenedb.com:24787" # os.environ.get("GRAPHENEDB_BOLT_URL")
+    graphenedb_user = "app149651838-8ERHph" # os.environ.get("GRAPHENEDB_BOLT_USER")
+    graphenedb_pass = "b.qFtgBSt5iSFJ.KN7p9aZXizU8YlL3" # os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+    driver = GraphDatabase.driver(graphenedb_url, auth=basic_auth(graphenedb_user, graphenedb_pass))
+    return driver
+
 # Serve React App
 @app.route('/', defaults={'path': ''}) 
 @app.route('/<path:path>') 
@@ -14,24 +21,24 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/api/connectdb')
-def connect(): # connect to neo4j instance
-    graphenedb_url = "bolt://hobby-ghjkfkgldghkgbkegepiladl.dbs.graphenedb.com:24787" # os.environ.get("GRAPHENEDB_BOLT_URL")
-    graphenedb_user = "app149651838-8ERHph" # os.environ.get("GRAPHENEDB_BOLT_USER")
-    graphenedb_pass = "b.qFtgBSt5iSFJ.KN7p9aZXizU8YlL3" # os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
-    driver = GraphDatabase.driver(graphenedb_url, auth=basic_auth(graphenedb_user, graphenedb_pass))
-
-    # test DB connection
-    session = driver.session()
-    session.run("CREATE (n:Person {name:'Bob'})")
-    result = session.run("MATCH (n:Person) RETURN n.name AS name")
-    for record in result:
-        print(record["name"])
-    session.close()
-
 @app.route('/api/createnode')
 def createNode():
-    return jsonify("hello")
+    # result = {''}
+    driver = connect()
+    with driver.session() as session:
+        session.run("CREATE (n:Person { name: 'Andy', title: 'Developer' })")
+
+
+@app.route('/api/getnodes')
+def getNodes():
+    names = {'names' : []}
+    driver = connect()
+    with driver.session() as session:
+        result = session.run("MATCH (n:Person) RETURN n.name AS name")
+        for record in result: # result is type BoltStatementResult
+            names['names'].append(record["name"])
+    return names
+    
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000)
--- client/src/screens/UpdateTree.js
+++ client/src/screens/UpdateTree.js
@@ -9,16 +9,24 @@ class UpdateTree extends React.Component {
             message : ''
         }
     }
+
     createNode = async () => { // async allows this function to be called asynchronously 
-        const response = await fetch('/api/createnode') //  await allows us to wait for the response of an asynchronous request.
+        const response = await fetch('/api/createnode') // await allows us to wait for the response of an asynchronous request.
+        // const myJson = await response.json()
+        // console.log(myJson)
+        // this.setState({message : myJson})
+    }
+
+    getNodes = async () => { 
+        const response = await fetch('/api/getnodes') 
         const myJson = await response.json()
-        this.setState({message : myJson})
+        console.log(myJson.names) // This is an array of node names
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.createNode}>Create Node </button>
+                <button onClick={this.getNodes}>Get Nodes</button>
                 {this.state.message}
             </div>
         )
