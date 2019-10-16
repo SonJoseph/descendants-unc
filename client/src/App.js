e19adcfbe3251ae9b66fbb4ca149244ce7de5079
annahattle <annalhattle@gmail.com>
2019-10-16 00:24:41 -0400

    added landing button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import Button from '@material-ui/core/Button'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function App() {
-  return  <h1> Welcome </h1>
+  return  <div>
+	  	<h1> The Descendants Project </h1>
+
+        <Link to = "/select" >
+          <Button  variant="outlined" color="primary" > View A Tree  </Button>
+        </Link>
+      
+	</div>
 }
 
 export default App;
