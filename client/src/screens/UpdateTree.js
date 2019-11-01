64631f95e48dbf85874671c95fc9bfcb5bc701c0
SonJoseph <joeson34@cs.unc.edu>
2019-10-31 23:41:39 -0400

    Divided update tree page into 2 adjustable pane. Added basic frontend functionality to add a node as a [specify relationship] to the selected node. Just need to create API for this now

--- client/src/screens/UpdateTree.js
+++ client/src/screens/UpdateTree.js
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
 import * as d3 from "d3"
-import _ from 'lodash';
-import dTree from 'd3-dtree';
-import './UpdateTree.css';
+import _ from 'lodash'
+import dTree from 'd3-dtree'
+import '../css/UpdateTree.css'
+import SplitPane from 'react-split-pane'
 window.d3 = d3;
 
 class UpdateTree extends React.Component {
@@ -16,7 +22,13 @@ class UpdateTree extends React.Component {
         super(props)
         this.state = {
             family : props.location.state.family,
-            tree : []
+            tree : [],
+            newPerson : {
+                name : "",
+                relnType : "",
+                relnOf : "" // ex: name is relnType of relnOf
+            },
+            confirm_msg : "" // for node creation
         }
     }
 
@@ -25,12 +37,6 @@ class UpdateTree extends React.Component {
         const myJson = await response.json()
         this.setState({tree : myJson})
         console.log(myJson)
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            name : e.target.value
-        })
     }
 
     componentDidMount() {
@@ -52,45 +58,102 @@ class UpdateTree extends React.Component {
 
    drawTree = () => {
      var treeData = this.state.tree
-      	dTree.init(treeData,{
-						target: this.refs.tree,
-						debug: true,
-						height: 800,
-						width: 1200,
-            margin: {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-  },
-  nodeWidth: 100,
-  styles: {
-    node: 'node',
-    linage: 'linage',
-    marriage: 'marriage',
-    text: 'nodeText'
-  }
-					});
+          dTree.init(treeData,
+            {
+                target: this.refs.tree,
+                debug: true,
+                height: 800,
+                width: 1200,
+                callbacks: {
+                    nodeClick: this.chooseNode
+                },
+                margin: {
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0
+                },
+                nodeWidth: 100,
+                styles: {
+                    node: 'node',
+                    linage: 'linage',
+                    marriage: 'marriage',
+                    text: 'nodeText'
+                }
+            }
+        );
    }
 
+    chooseNode = (name, extra, id) => {
+        // button event for selecting a node
+        this.setState({
+            newPerson : {
+                relnOf : name,
+                name : this.state.newPerson.name,
+                relnType : this.state.newPerson.relnType
+            }
+        })
+   }
+
+    typeName = (e) => {
+        // input handler for name of new person
+        this.setState({
+            newPerson : {
+                name : e.target.value,
+                relnType : this.state.newPerson.relnType,
+                relnOf : this.state.newPerson.relnOf 
+            }
+        })
+        console.log(e.target.value)
+    }
+
+    selectRelationship = (e) => {
+        // input handler for selecting relationship of new person to selected node
+        this.setState({
+            newPerson : {
+                name : this.state.newPerson.name,
+                relnType : e.target.value,
+                relnOf : this.state.newPerson.relnOf
+            }
+        })
+        console.log(this.state.newPerson.relnType);
+    }
+
+    createNode = () => {
+        this.setState({confirm_msg : 'Are you sure you want to add ' + this.state.newPerson.name + ' as a ' 
+        + this.state.newPerson.relnType + ' of ' + this.state.newPerson.relnOf + '?'});
+    }
 
     render() {
         return (
             <Container>
-            <Container>
-                {this.state.family}
-                <TextField
-                    label="Name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                />
-                <Button onClick={this.getTree}>Get Tree data</Button>
-                <Button onClick={this.drawTree}>Draw Node</Button>
-            </Container>
-            <Container>
-                <svg ref="tree" id = "graph" width={800} height={500}></svg>
-            </Container>
 
+            <SplitPane split="vertical" defaultSize={350}>
+                        <div>
+                            Selected Person: {this.state.newPerson.relnOf}
+                            <TextField
+                                label="New Person"
+                                value={this.state.newPerson.name}
+                                onChange={this.typeName}
+                            />
+                            <InputLabel >What is {this.state.newPerson.name}'s relationship to {this.state.newPerson.relnOf}? </InputLabel>
+                            <Select
+                                value={this.state.newPerson.relnType}
+                                onChange={this.selectRelationship}
+                            >
+                                <MenuItem value={'spouse'}>Spouse</MenuItem>
+                                <MenuItem value={'child'}>Child</MenuItem>
+                            </Select>
+                            <Button onClick={this.createNode}>Create Node</Button>
+                            {this.state.confirm_msg}
+                        </div>
+                        <div>
+                            {this.state.family}'s Family
+                            <svg ref="tree" id = "graph" width={800} height={500}></svg>
+                            <Button onClick={this.getTree}>Get Tree data</Button>
+                            <Button onClick={this.drawTree}>Draw Node</Button>
+                        </div>
+            </SplitPane>
 
             </Container>
 
