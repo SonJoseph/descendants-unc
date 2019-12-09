7ed56fde999144fe5ceea6b5c3cfbd18a22cc856
jade457 <jade.wu86@gmail.com>
2019-12-09 02:06:07 -0500

    more changes?

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -14,15 +14,17 @@ class Alert extends React.Component {
       return(
         <Popup trigger={<Grid container justify="center" alignItems="center"><Grid item><Button variant="contained" size="large" style={{margin: 8}}> Delete This Person </Button></Grid></Grid>} position="right center" modal>
           {close => (
-              <div>
-                <Typography variant="h6"> Are you sure you want to delete this person from the tree? </Typography>
-                <Typography variant="body2"> This will delete any people directly below this person in the tree, their spouse, and the data for all of those people.
+              <Grid container style = {{margin: '15px'}}>
+              <Grid item>
+                <Typography variant="h6" style={{color: "#c62828", margin: '15px'}}> Are you sure you want to delete this person from the tree? </Typography>
+                <Typography variant="body2" style={{margin: '15px'}}> This will delete any people directly below this person in the tree, their spouse, and the data for all of those people.
                   </Typography>
-                <div className="actions">
-                  <Button className="button" onClick={() => { close(); }}> Cancel </Button>
-                  <Button className="button" variant="outlined" color="primary" onClick={() => { this.props.delete() }}> Confirm </Button>
-                </div>
-              </div>
+              </Grid>
+                <Grid item className="actions">
+                  <Button className="button" onClick={() => { close(); }} style = {{margin: '8px'}}> Cancel </Button>
+                  <Button className="button" variant="outlined" color="primary" onClick={() => { this.props.delete() }} style = {{margin: '8px'}}> Confirm </Button>
+                </Grid>
+              </Grid>
           )}
         </Popup>
       )
