3990bb49afcc218c605f6add95eb418ec7ca63ba
SonJoseph <joeson34@cs.unc.edu>
2019-12-08 21:15:35 -0500

    defaults to click node to view more message

---
+++ client/src/components/Message.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Message extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            
+                <h1>
+                    {this.props.message}
+                </h1>
+            
+        )
+    }
+}
+
+export default Message
